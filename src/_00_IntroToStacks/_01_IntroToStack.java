59972112ceac899b0a29a66ae128c93296d125b7
augie <augie@keatinge.com>
2023-03-14 14:15:08 -0700

    working on itr

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -1,6 +1,7 @@
 package _00_IntroToStacks;
 
 import java.util.Random;
+
 import java.util.Stack;
 
 import javax.swing.JOptionPane;
@@ -9,8 +10,18 @@ public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
-
+    	 Stack<Double> stack = new Stack<Double>();
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
+for (int i = 0; i < 99; i++) {
+	Random rand = new Random();
+	rand.doubles();
+
+	
+}
+JOptionPane.showMessageDialog(null, "Please enter two numbers:");
+
+int num1 = Integer.parseInt(JOptionPane.showInputDialog("Enter a number between 0 and 100")); 
+int num2 = Integer.parseInt(JOptionPane.showInputDialog("Enter a number between 0 and 100")); 
 
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
 
