2158f74176e89e933d61dea66e0369bdbebb042e
opie10 <40475532+opie10@users.noreply.github.com>
2023-05-08 16:58:24 -0700

    donish?
    done hangman concept, want to add actual hangman part of hangman, so do at home/school?

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -11,45 +11,105 @@ import javax.swing.JPanel;
 public class Hangman {
 	public static void main(String[] args) {
 		Boolean game = false;
+		
 		JLabel jl = new JLabel();
 		JFrame jf = new JFrame();
 		JPanel jp = new JPanel();
-		jf.add(jl);
-		jp.add(jf);
-		jp.setSize(100, 100);
+		jp.add(jl);
+		jf.add(jp);
+		jf.setSize(500, 500);
 		Stack<String> words = new Stack<String>();
 		int num = Integer.parseInt(JOptionPane.showInputDialog("How many words would you like to guess?"));
 		for (int i = 0; i < num; i++) {
 			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
 
-			if (words.contains(words.get(i))) {
+			if (words.contains(words.push(Utilities.readRandomLineFromFile("dictionary.txt")))) {
 				words.pop();
-				i = i - 1;
+				
+			
 			}
+			
 			game = true;
 		}
 		String guessword = words.pop();
-		while (game = true) {
-			jp.setVisible(true);
+		
+		if (game == true) {
+			int lives = guessword.length();
+			
+			jf.setVisible(true);
 			String jltext = "";
 			for (int i = 0; i < guessword.length(); i++) {
-				jltext = jl + "_";
+				jltext = jltext + "_";
 				jl.setText(jltext);
 			}
+			while(lives>0) {
 			String guessedletter = JOptionPane.showInputDialog("Guess a letter").toString();
-			if (guessedletter.length() < 1) {
+			System.out.println(lives);
+			if (guessedletter.length() == 1) {
 				char[] guesschar = guessedletter.toCharArray();
 				for (int i = 0; i < guessword.length(); i++) {
 					if (guessword.charAt(i) == guesschar[0]) {
-				
+						char[] jltextar = jltext.toCharArray();
+						jltextar[i]=guesschar[0];
 						
-jl.setText(jltext);
+						jltext="";
+					for (int j = 0; j < jltextar.length; j++) {
+						
+						jltext = jltext+jltextar[j];
+						
 					}
+						
+						jl.setText(jltext);
+					}
+					
+				}
+				if (guessword.contains(guessedletter)==false) {
+					lives = lives-1;
 				}
 			}
 
 			else {
+				
 				guessedletter = JOptionPane.showInputDialog("Guess another letter (ONE LETTER)").toString();
+			}
+			if (lives==0) {
+				
+				int reply =JOptionPane.showConfirmDialog(null, "Do you want to coninue playing?");
+				 if (reply == JOptionPane.YES_OPTION)
+	                {
+	                    jltext ="";
+	                    jl.setText(jltext);
+	                    if (words.size()>0) {
+							guessword=words.pop();
+							 lives=guessword.length();
+							 for (int i = 0; i < guessword.length(); i++) {
+									jltext = jltext + "_";
+									jl.setText(jltext);
+								}
+							 
+						}
+	                    else {
+	                    	words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+	                    	guessword=words.pop();
+	                    	lives=guessword.length();
+	                    	for (int i = 0; i < guessword.length(); i++) {
+	            				jltext = jltext + "_";
+	            				jl.setText(jltext);
+	            				
+	            			}
+	                    	
+	                    }
+	                   
+	                   
+	                }
+				 if (reply == JOptionPane.NO_OPTION||reply==JOptionPane.CANCEL_OPTION)
+	                {
+					 System.exit(0);
+	                }
+	                
+			
+			}
+		
 			}
 		}
 	}
